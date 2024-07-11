5a4872a31c55168cf4197fa7551abd40172dc6a3
ShotursunovShomurod <ziyodxon.i08@gmail.com>
2024-07-12 01:22:10 +0500

    hammasi tayor

--- db/server.js
+++ db/server.js
@@ -1,15 +1,15 @@
 export const USERS = [
-    {
-        id: 121212121212,
-        name: "John",
-        username: "john32",
-        password: 12345678
-    },
-    {
-        id: 121212121211,
-        name: "Selena",
-        username: "Selena32",
-        password: 87654321
-    }
+    // {
+    //     id: 121212121212,
+    //     name: "John",
+    //     username: "john32",
+    //     password: 12345678
+    // },
+    // {
+    //     id: 121212121211,
+    //     name: "Selena",
+    //     username: "Selena32",
+    //     password: 87654321
+    // }
 
 ]
--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 import { USERS } from "../db/server.js";
 
 const btnOpen = document.querySelector(".btn__open");
@@ -15,112 +14,109 @@ const modelPasswordConfirm = document.querySelector(".model__password-confirm");
 const wrapper = document.querySelector(".wrapper");
 
 function creatElement(data) {
-  while (wrapper.firstChild) {
-    wrapper.firstChild.remove();
-  }
-  data.forEach((user) => {
-    let card = document.createElement("div");
-    card.innerHTML = `
+    while (wrapper.firstChild) {
+        wrapper.firstChild.remove();
+    }
+    data.forEach((user) => {
+        let card = document.createElement("div");
+        card.classList.add("card");
+        card.innerHTML = `
       <div class="card__circle"></div>
       <h3>${user.name}</h3>
       <p>${user.username}</p>
       <p>${user.password}</p>`;
-    wrapper.appendChild(card);
-  });
+        wrapper.appendChild(card);
+    });
 }
 creatElement(USERS);
 
 model.addEventListener("submit", (event) => {
-  event.preventDefault();
-  let name = modelName.value;
-  let username = modelUsername.value;
-  let password = modelPassword.value;
-  let passwordConfirm = modelPasswordConfirm.value;
+    event.preventDefault();
+    let name = modelName.value;
+    let username = modelUsername.value;
+    let password = modelPassword.value;
+    let passwordConfirm = modelPasswordConfirm.value;
 
-  // Clear previous error borders
-  modelPassword.style.border = "";
-  modelPasswordConfirm.style.border = "";
+    // Clear previous error borders
+    modelPassword.style.border = "";
+    modelPasswordConfirm.style.border = "";
 
-  let existUser = USERS.findIndex((user) => user.username === username);
-  if (existUser >= 0) {
-    return alert("Bu username mavjud");
-  }
-  if (password !== passwordConfirm) {
-    modelPassword.style.border = "1px solid red";
-    modelPasswordConfirm.style.border = "1px solid red";
-    return alert("Parol bir xil emas");
-  }
+    let existUser = USERS.findIndex((user) => user.username === username);
+    if (existUser >= 0) {
+        return alert("Bu username mavjud");
+    }
+    if (password !== passwordConfirm) {
+        modelPassword.style.border = "1px solid red";
+        modelPasswordConfirm.style.border = "1px solid red";
+        return alert("Parol bir xil emas");
+    }
 
-  let newUser = {
-    id: new Date().getTime(),
-    name,
-    username,
-    password,
-  };
+    let newUser = {
+        id: new Date().getTime(),
+        name,
+        username,
+        password,
+    };
 
-  USERS.push(newUser);
-  console.log(USERS);
+    USERS.push(newUser);
+    console.log(USERS);
 
-  model.reset();
-  popap.style.display = "none";
-  creatElement(USERS);
+    model.reset();
+    popap.style.display = "none";
+    creatElement(USERS);
 });
 
 btnOpen.addEventListener("click", () => {
-  popap.style.display = "flex";
+    popap.style.display = "flex";
 });
 
 btnClose.addEventListener("click", () => {
-  popap.style.display = "none";
+    popap.style.display = "none";
 });
 
 overlay.addEventListener("click", () => {
-  popap.style.display = "none";
+    popap.style.display = "none";
 });
 
-// Toggle password visibility and icons
 function togglePasswordVisibility(passwordInput, openEyeBtn, backEyeBtn) {
-  if (passwordInput.type === "password") {
-    passwordInput.type = "text";
-    openEyeBtn.style.display = "none";
-    backEyeBtn.style.display = "inline";
-  } else {
-    passwordInput.type = "password";
-    openEyeBtn.style.display = "inline";
-    backEyeBtn.style.display = "none";
-  }
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+        openEyeBtn.style.display = "none";
+        backEyeBtn.style.display = "inline";
+    } else {
+        passwordInput.type = "password";
+        openEyeBtn.style.display = "inline";
+        backEyeBtn.style.display = "none";
+    }
 }
 
-// Show/hide eye icons based on input
 function updateEyeIcons(passwordInput, openEyeBtn, backEyeBtn) {
-  if (passwordInput.value.length > 0) {
-    openEyeBtn.style.display = "inline";
-    backEyeBtn.style.display = "none";
-  } else {
-    openEyeBtn.style.display = "none";
-    backEyeBtn.style.display = "none";
-  }
+    if (passwordInput.value.length > 0) {
+        openEyeBtn.style.display = "inline";
+        backEyeBtn.style.display = "none";
+    } else {
+        openEyeBtn.style.display = "none";
+        backEyeBtn.style.display = "none";
+    }
 }
 
-// Add event listeners for password input fields
 document.querySelectorAll(".texts").forEach((textWrapper) => {
-  const passwordInput = textWrapper.querySelector("input[type='password']");
-  const openEyeBtn = textWrapper.querySelector(".openeye");
-  const backEyeBtn = textWrapper.querySelector(".backeye");
+    const passwordInput = textWrapper.querySelector("input[type='password']");
+    const openEyeBtn = textWrapper.querySelector(".openeye");
+    const backEyeBtn = textWrapper.querySelector(".backeye");
 
-  // Initially hide both eye icons
-  openEyeBtn.style.display = "none";
-  backEyeBtn.style.display = "none";
+    openEyeBtn.style.display = "none";
+    backEyeBtn.style.display = "none";
 
-  passwordInput.addEventListener("input", () => {
-    updateEyeIcons(passwordInput, openEyeBtn, backEyeBtn);
-  });
+    passwordInput.addEventListener("input", () => {
+        updateEyeIcons(passwordInput, openEyeBtn, backEyeBtn);
+    });
 
-  openEyeBtn.addEventListener("click", () => {
-    togglePasswordVisibility(passwordInput, openEyeBtn, backEyeBtn);
-  });
+    openEyeBtn.addEventListener("click", () => {
+        togglePasswordVisibility(passwordInput, openEyeBtn, backEyeBtn);
+    });
 
-  backEyeBtn.addEventListener("click", () => {
-    togglePasswordVisibility(passwordInput, openEyeBtn, backEyeBtn);
-  });
+    backEyeBtn.addEventListener("click", () => {
+        togglePasswordVisibility(passwordInput, openEyeBtn, backEyeBtn);
+    });
 });
